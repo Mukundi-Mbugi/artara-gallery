2b16d7aa70fb4b9d42f7aedd5120c5ff978aba3f
Elijah Mbugi <mukundielijah@gmail.com>
2022-08-03 23:12:29 +0300

    build models

--- app/models/artist.rb
+++ app/models/artist.rb
@@ -1,2 +1,7 @@
 class Artist < ApplicationRecord
+    has_secure_password
+    has_many :items
+    validates :name, presence: true
+    validates :email, presence: true, uniqueness: true
+    validates :password, presence: true, length: { minimum: 6 }
 end
--- app/models/item.rb
+++ app/models/item.rb
@@ -1,2 +1,6 @@
 class Item < ApplicationRecord
+    belongs_to :artist
+    validates :title, presence: true
+    validates :description, presence: true
+    validates :image_url, presence: true
 end
