1b463ca3c5195d6d583452ba7b04d584adcf4077
Elijah Mbugi <mukundielijah@gmail.com>
2022-08-04 00:35:25 +0300

    create item logic

--- client/src/components/CreateItem/CreateItem.js
+++ client/src/components/CreateItem/CreateItem.js
@@ -1,31 +1,79 @@
-import React from 'react';
-import {useState} from 'react';
-import "./CreateItem.css"
+import React from "react";
+import { useState } from "react";
+import { useHistory } from "react-router";
+import "./CreateItem.css";
 
 function CreateItem() {
-    const [title, setTitle] = useState('');
-    const [image, setImage] = useState('');
-    const [description, setDescription] = useState('');
+  const [title, setTitle] = useState("");
+  const [image_url, setImage_url] = useState("");
+  const [description, setDescription] = useState("");
+  const [isLoading, setIsLoading] = useState(false);
+  const [errors, setErrors] = useState("")
+  const history = useHistory();
+
+  function handleCreateItem(e){
+    e.preventDefault();
+    setIsLoading(true);
+    fetch("/recipes", {
+      method: "POST",
+      headers: {
+        "Content-Type": "application/json",
+      },
+      body: JSON.stringify({
+        title,
+        image_url,
+        description
+      }),
+    }).then((r) => {
+      setIsLoading(false);
+      if (r.ok) {
+        history.push("/");
+      } else {
+        r.json().then((err) => setErrors(err.errors));
+      }
+    });
+  }
   return (
     <div>
-        <div className="create-form">
-            <div className="form-group">
-                <label htmlFor="title">Title</label>
-                <input type="text"  id="title" value={title} onChange={(e) => setTitle(e.target.value)} />
-                </div>
-            <div className="form-group">
-                <label htmlFor="image">Image</label>
-                <input type="text"  id="image" value={image} onChange={(e) => setImage(e.target.value)} />
-                </div>
-            <div className="form-group">
-                <label htmlFor="description">Description</label>
-                <input type="text" id="description" value={description} onChange={(e) => setDescription(e.target.value)} />
-                <button type="submit" className="submit-create-btn">Submit</button>
-                </div>
-            
+      <div className="create-form">
+        <div className="form-group">
+          <label htmlFor="title">Title</label>
+          <input
+            type="text"
+            id="title"
+            value={title}
+            onChange={(e) => setTitle(e.target.value)}
+          />
         </div>
+        <div className="form-group">
+          <label htmlFor="image">Image</label>
+          <input
+            type="text"
+            id="image"
+            value={image_url}
+            onChange={(e) => setImage_url(e.target.value)}
+          />
+        </div>
+        <div className="form-group">
+          <label htmlFor="description">Description</label>
+          <input
+            type="text"
+            id="description"
+            value={description}
+            onChange={(e) => setDescription(e.target.value)}
+          />
+          <button type="submit" className="submit-create-btn" onClick={handleCreateItem}>
+          {isLoading ? "Loading..." : "Submit Item"}
+          </button>
+          <div>
+            {errors.map((err) => (
+              <p key={err}>{err}</p>
+            ))}
+          </div>
+        </div>
+      </div>
     </div>
-  )
+  );
 }
 
-export default CreateItem
+export default CreateItem;
