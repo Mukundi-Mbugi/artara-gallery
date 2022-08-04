09cc1af6ad32fa6a1cdc38e4210dfe067019beca
Elijah Mbugi <mukundielijah@gmail.com>
2022-08-04 16:16:00 +0300

    artists fetch and style

--- client/src/components/Artists/Artists.js
+++ client/src/components/Artists/Artists.js
@@ -1,12 +1,36 @@
 import React from 'react'
 import Footer from '../Footer/Footer'
 import Navbar from '../Navbar/Navbar'
+import {useEffect, useState} from 'react'
+import "./Artists.css"
+
+
 
 function Artists() {
+  const [artists, setArtists] = useState([]);
+
+  useEffect(() => {
+    fetch('/artists')
+      .then(res => res.json())
+      .then(data => {
+        console.log(data);
+        setArtists(data)
+      }
+      )
+  }, [])
+
   return (
     <>
     <Navbar />
-    <div>Artists</div>
+    <div>
+      {artists.map(artist => (
+        <div key={artist.id} className="artist-div">
+          <h3>{artist.name}</h3>
+          <p>{artist.email}</p>
+          <button><a href={`mailto:${artist.email}`}>Contact me</a> </button>
+          </div>
+      ))}
+    </div>
     <Footer />
     </>
   )
