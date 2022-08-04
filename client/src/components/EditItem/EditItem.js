dc612fc2145a402aa7002e399b9b5ab0cac95d41
Elijah Mbugi <mukundielijah@gmail.com>
2022-08-04 22:05:32 +0300

    delete method

--- client/src/components/EditItem/EditItem.js
+++ client/src/components/EditItem/EditItem.js
@@ -1,18 +1,28 @@
-function EditItem({art, onDeleteArt, onUpdateArt}) {
-    return (
-        <div>
-            <div className="card">
-                <div className="card-image">
-                    <h4>{art.title}</h4>
-                    <img src={art.url} alt="avatar" id="avatar" />
-                    <p>{art.description}</p>
-                    </div>
-                    <div className="card-buttons">
-                    <button className="delete-btn" onClick={() => onDeleteArt(art.id)}>delete</button>
-                    <button className="edit-btn" onClick={() => onUpdateArt(art.id)}>edit</button>
-                    </div>
-                    </div>
+function EditItem({ art, onDeleteArt, onUpdateArt }) {
+    const [showForm, setShowForm] = useState(false);
+    function handleDelete(){
+        fetch(`/edit/${art.id}`, {
+            method: "DELETE",
+        }).then((r) => {
+            if (r.ok) {
+                onDeleteArt(art.id);
+            }
+        });
+    }
+  return (
+    <div>
+      <div className="card">
+        <div className="card-image">
+          <h4>{art.title}</h4>
+          <img src={art.url} alt="avatar" id="avatar" />
+          <p>{art.description}</p>
         </div>
-    )
+        <div className="card-buttons">
+          <i class="material-icons" onClick={handleDelete}>delete</i>
+          <i class="material-icons"onClick={handleEdit}>edit</i>
+        </div>
+      </div>
+    </div>
+  );
 }
 export default EditItem;
