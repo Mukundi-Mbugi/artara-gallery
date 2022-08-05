44a5272f109106bef2e77d0f6b35c4e7deb35308
Elijah Mbugi <mukundielijah@gmail.com>
2022-08-05 07:49:36 +0300

    update form styles

--- client/src/components/UpdateForm/UpdateForm.js
+++ client/src/components/UpdateForm/UpdateForm.js
@@ -1,50 +1,65 @@
 import React from "react";
 import { useState } from "react";
+import "./UpdateForm.css";
 
 function UpdateForm({ art, setShowForm, onUpdateArt }) {
-    const [title, setTitle] = useState(art.title);
-    const [image_url, setImage_url] = useState(art.image_url);
-    const [description, setDescription] = useState(art.description);
+  const [title, setTitle] = useState(art.title);
+  const [image_url, setImage_url] = useState(art.image_url);
+  const [description, setDescription] = useState(art.description);
 
-
-    function handleUpdate(){
-        fetch(`/items/${art.id}`, {
-            method: "PUT",
-            headers: {
-                "Content-Type": "application/json",
-            },
-            body: JSON.stringify({
-                title,
-                image_url,
-                description,
-            }),
-        }).then((res) => res.json())
-        .then((updatedArt) => {
-          onUpdateArt(updatedArt);
-          setShowForm(false)
-
-        });
-    }
-    return (
-        <div>
-            <div className="card-form">
-            <div className="form-group">
-                <label htmlFor="title">Title</label>
-                <input type="text" id="title" value={title} onChange={(e) => setTitle(e.target.value)} />
-                </div>
-                <div className="form-group">
-                <label htmlFor="image">Image Url</label>
-                <input type="text" id="image" value={image_url} onChange={(e) => setImage_url(e.target.value)} />
-                </div>
-                <div className="form-group">
-                <label htmlFor="description">Description</label>
-                <input type="text" id="description" value={description} onChange={(e) => setDescription(e.target.value)} />
-                </div>
-                <div className="form-group">
-                <button onClick={handleUpdate}>Update</button>
-                </div>
-                </div>
+  function handleUpdate() {
+    fetch(`/items/${art.id}`, {
+      method: "PUT",
+      headers: {
+        "Content-Type": "application/json",
+      },
+      body: JSON.stringify({
+        title,
+        image_url,
+        description,
+      }),
+    })
+      .then((res) => res.json())
+      .then((updatedArt) => {
+        onUpdateArt(updatedArt);
+        setShowForm(false);
+      });
+  }
+  return (
+    <div>
+      <div className="card-form">
+        <div className="form-group">
+          <label htmlFor="title">Title</label>
+          <input
+            type="text"
+            id="title"
+            value={title}
+            onChange={(e) => setTitle(e.target.value)}
+          />
         </div>
-    )
+        <div className="form-group">
+          <label htmlFor="image">Image Url</label>
+          <input
+            type="text"
+            id="image"
+            value={image_url}
+            onChange={(e) => setImage_url(e.target.value)}
+          />
+        </div>
+        <div className="form-group">
+          <label htmlFor="description">Description</label>
+          <input
+            type="textArea"
+            id="description"
+            value={description}
+            onChange={(e) => setDescription(e.target.value)}
+          />
+        </div>
+        <div className="form-group">
+          <button onClick={handleUpdate}>Update</button>
+        </div>
+      </div>
+    </div>
+  );
 }
 export default UpdateForm;
